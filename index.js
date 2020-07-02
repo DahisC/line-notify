39e1bf3d30b6278a57813a2e20eba06993e126f1
DahisC <dahischeng@gmail.com>
2020-07-02 13:41:20 +0800

    v1.0.1

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 'use strict';
 const router = require('express').Router();
 
-let base = {
-  auth_route: undefined,
-};
+// let base = {
+//   auth_route: undefined,
+// };
 
-let authorizeParams = {
-  response_type: 'code',
-  client_id: undefined,
-  redirect_uri: undefined,
-  scope: 'notify',
-  state: undefined,
-};
+// let authorizeParams = {
+//   response_type: 'code',
+//   client_id: undefined,
+//   redirect_uri: undefined,
+//   scope: 'notify',
+//   state: undefined,
+// };
 
-let tokenParams = {
-  grant_type: 'authorization_code',
-  code: undefined,
-  redirect_uri: undefined,
-  client_id: undefined,
-  client_secret: undefined,
-};
+// let tokenParams = {
+//   grant_type: 'authorization_code',
+//   code: undefined,
+//   redirect_uri: undefined,
+//   client_id: undefined,
+//   client_secret: undefined,
+// };
 
-exports.lineNotify = function ({
-  client_id,
-  client_secret,
-  redirect_uri,
-  state,
-}) {
+exports.lineNotify = function ({ client_id }) {
+  console.log(client_id);
   router.get('/line-notify/subscribe', (req, res) => {
     console.log(req.query);
     res.send('Step 1 authorize');
